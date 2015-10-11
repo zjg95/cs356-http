1b37f86cc07536399bedb6e9dea72af8f664d6cc
Zachary Goodman <zjgoodman@utexas.edu>
2015-10-10 20:26:54 -0500

    added sample client-server files

---
+++ TCPclient.py
@@ -0,0 +1,16 @@
+from socket import *
+import sys
+
+serverName = 'localhost'
+serverPort = 12000
+
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName,serverPort))
+
+sentence = input('Input lowercase sentence: ')
+
+clientSocket.send(sentence.encode())
+modifiedSentence = clientSocket.recv(2048)
+
+print('From Server: ' + modifiedSentence.decode())
+clientSocket.close()
